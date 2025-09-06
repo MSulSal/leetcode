080da13087bb2221c92c4c4edd9ee64be2a52542
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-09-06 12:58:19 -0500

    contains duplicate

---
+++ arrays-and-hashing/contains-duplicate.py
@@ -0,0 +1,31 @@
+'''
+||| PROBLEM|||
+
+Given an integer array nums, 
+return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+||| SOLUTION |||
+'''
+from types import List
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    # set of nums to store unique nums
+    numset = set()
+
+    # loop through nums and if current num already in numset, return True for
+    # containing duplicate, else add num to numset. If nums fully looped through, return False.
+    for num in nums:
+        if num in numset:
+            return True
+        numset.add(num)
+
+    return False
+
+'''
+n: len(nums)
+
+Time Complexity: O(n) [single for loop]
+
+Space Complexity: O(n) [len(numset) <= n]
+'''
